3663906a84838ae4bcdf83317226b7c2fe91a87c
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-04-18 20:52:40 -0300

    updated

--- Api-Geo/Program.cs
+++ Api-Geo/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Build Database
+var connectionString = builder.Configuration.GetConnectionString("IconsConnection");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
